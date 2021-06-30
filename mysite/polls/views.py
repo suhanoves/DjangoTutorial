fa337c38c433c034a795752f76878339198e7c58
Eugene Suhanov <suhanoves@gmail.com>
2021-06-30 16:46:28 +0300

    Create results view and template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -19,8 +19,11 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/results.html', context=context)
 
 
 def vote(request, question_id):
